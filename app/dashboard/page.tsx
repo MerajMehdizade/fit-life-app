d394f59cb22c71ce8a39d54d713499837868ba5e
merajmehdizade <mdzmeraj8@gmail.com>
2025-11-20 23:35:04 +0330

    update code change forms add toast

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,16 +6,78 @@ import { useRouter } from "next/navigation";
 export default function DashboardPage() {
   const router = useRouter();
   const [user, setUser] = useState<{ name: string } | null>(null);
+  const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     const userData = localStorage.getItem("user");
 
-    if (!token || !userData) router.push("/login");
-    else setUser(JSON.parse(userData));
+    if (!token || !userData) {
+      router.push("/login");
+    } else {
+      setUser(JSON.parse(userData));
+      setShowToast(true);
+      const timer = setTimeout(() => setShowToast(false), 5000);
+      return () => clearTimeout(timer);
+    }
   }, []);
 
-  if (!user) return <p className="text-center mt-10">Loading...</p>;
+  if (!user) return <div className="flex items-center justify-center h-screen"><p className=" text-white text-3xl">...Loading</p></div>;
 
-  return <h1 className="text-2xl text-center mt-10 text-white">Welcome, {user.name} to Dashbord</h1>;
+  return (
+    <div className="relative min-h-screen flex flex-col items-center justify-start pt-10 bg-gray-900 w-full">
+      {showToast && (
+        <div dir="rtl"
+          className="flex items-center w-full max-w-sm p-4 text-body bg-gray-100 dark:bg-gray-700 rounded-md shadow border border-gray-300 dark:border-gray-600 text-white fixed bottom-5 right-5"
+          role="alert"
+        >
+          <svg
+            className="w-5 h-5 text-blue-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m12 18-7 3 7-18 7 18-7-3Zm0 0v-5"
+            />
+          </svg>
+
+          <div className="mr-3 border-r border-gray-300 pr-3 dark:border-gray-500 capitalize">
+            {user.name} عزیز خوش آمدید
+          </div>
+
+          <button
+            type="button"
+            className="ms-auto flex items-center justify-center text-body border border-transparent  font-medium rounded text-sm h-8 w-8 focus:outline-none focus:ring-2 text-red-500 cursor-pointer"
+            onClick={() => setShowToast(false)}
+            aria-label="Close"
+          >
+            <span className="sr-only">Close</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 17.94 6M18 18 6.06 6"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
+
+
+    </div>
+  );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,94 +24,93 @@ export default function LoginPage() {
     } else alert(data.message);
   };
   return (
-    <div className=" text-white flex min-h-screen flex-col items-center pt-16 sm:justify-center sm:pt-0">
-      <div className="relative mt-12 max-w-lg sm:mt-10 ">
-        <div className="relative -mb-px h-px w-full from-transparent via-sky-300 to-transparent "
-        ></div>
-        <div
-          className="mx-5 border dark:border-b-white/50 dark:border-t-white/50 border-b-white/20 sm:border-t-white/20  border-white/20 border-l-white/20 border-r-white/20 rounded-xl">
-          <div className="flex flex-col p-6" dir="rtl">
-            <h3 className="text-xl  leading-6 tracking-tighter">ورود</h3>
-            <p className="mt-2 text-sm  text-white/50">خوش برگشتی! لطفاً برای ادامه، اطلاعات ورود خود را وارد کنید.
-            </p>
+   
+    <section className="bg-white dark:bg-gray-900">
+      <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
+        <form onSubmit={handleSubmit} className="w-full max-w-md">
+          <div className="flex justify-center mx-auto">
+            <img className="w-auto h-24 sm:h-32" src="/logoFitness.svg" alt="" />
           </div>
-          <div className="p-6 pt-0">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
 
-              <div className="relative z-0 w-full mb-5">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  onChange={e => setEmail(e.target.value)} value={email}
-                  placeholder=" "
-                  required
-                  className="block py-2.5 px-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer text-left"
-                />
-                <label
-                  htmlFor="email"
-                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-top-left peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-200 right-0"
-                >
-                  ایمیل
-                </label>
-              </div>
-              <div className="relative z-0 w-full mb-5">
-                <input
-                  name="password"
-                  id="password"
-                  type={showPassword ? "text" : "password"}
-                  onChange={e => setPassword(e.target.value)}
-                  placeholder=" "
-                  required
-                  className="block py-2.5 pr-2 pl-0 w-full text-sm text-gray-200 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-500 peer "
-                />
+          <div className="flex items-center justify-center mt-6" dir="rtl">
+            <a href="./login" className="w-1/3 pb-4 font-medium text-center text-gray-800 capitalize border-b-2 border-blue-500 dark:border-blue-400 dark:text-white">
+              ورود
+            </a>
 
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(prev => !prev)}
-                  className="absolute top-1/2 left-0 transform -translate-y-1/2 text-gray-400 hover:text-white"
-                >
-                  {showPassword ? (
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                    </svg>
-                  ) : (
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
-                  )}
-                </button>
-
-                <label
-                  htmlFor="password"
-                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-top-right peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:text-gray-200 right-0"
-                >
-                  رمز عبور
-                </label>
-              </div>
-              {/* 
-              <div className="mt-4 flex items-center justify-between">
-                <label className="flex items-center gap-2">
-                  <input type="checkbox" name="remember"
-                    className="outline-none focus:outline focus:outline-sky-300" />
-                  <span className="text-xs">به یاد داشته باش</span>
-                </label>
-                <a className="text-sm  text-foreground underline" href="/forgot-password">فراموشی رمز عبور</a>
-              </div> */}
-              <div className="mt-4 flex items-center justify-end gap-x-2">
-                <a className="inline-flex items-center justify-center rounded-md text-sm  transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:ring hover:ring-white h-10 px-4 py-2 duration-200"
-                  href="./register">عضویت در فیت</a>
-                <button
-                  className=" hover:bg-zinc-950 hover:text-white hover:ring hover:ring-zinc-600 transition duration-300 inline-flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black h-10 px-4 py-2"
-                  type="submit">ورود به پنل کاربری</button>
-
-
-              </div>
-            </form>
+            <a href="./register" className="w-1/3 pb-4 font-medium text-center text-gray-500 capitalize border-b dark:border-gray-400 dark:text-gray-300">
+              عضویت
+            </a>
           </div>
-        </div>
+
+
+
+          <div className="relative flex items-center mt-6">
+            <span className="absolute">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              </svg>
+            </span>
+
+            <input
+              id="email"
+              type="text"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="ایمیل"
+              required
+              className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+
+          <div className="relative flex items-center mt-4">
+            <span className="absolute left-0">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+              </svg>
+            </span>
+
+            <input
+              id="password"
+              type={showPassword ? "text" : "password"}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required
+              placeholder="رمز عبور"
+              className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(p => !p)}
+              className="absolute right-3 text-gray-400 hover:text-white"
+            >
+              {showPassword ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 01-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+            >
+              ورود به پنل کاربری
+            </button>
+            <div className="mt-6 text-center">
+              <a href="./register" className="text-sm text-blue-500 hover:underline dark:text-blue-400">
+                ثبت نام در فیت
+              </a>
+            </div>
+          </div>
+        </form>
       </div>
-    </div>
+    </section>
   );
 }
