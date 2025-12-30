18d65326678876fe9e36205ca71dfc427c8d43a9
merajmehdizade <mdzmeraj8@gmail.com>
2025-12-30 18:30:22 +0330

    add complete-profile Ui and Ux

--- app/dashboard/admin/layout.tsx
+++ app/dashboard/admin/layout.tsx
@@ -2,22 +2,19 @@ import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/lib/getUser";
 import AdminShell from "./_components/AdminShell";
 import { ReactNode } from "react";
+import User from "@/models/User";
+import dbConnect from "@/lib/db";
 
 export default async function AdminLayout({ children }: { children: ReactNode }) {
-  const user = await getCurrentUser();
+  const jwtUser = await getCurrentUser();
+  if (!jwtUser) redirect("/login");
 
+  await dbConnect();
+  const user = await User.findById(jwtUser.userId);
   if (!user) redirect("/login");
 
-  const role = user.role ?? user.user?.role ?? null;
-  if (role !== "admin") redirect(`/dashboard/${role ?? "student"}`);
+  if (user.role !== "admin") redirect(`/dashboard/${user.role ?? "student"}`);
 
-
-
-  return (
-    <>
-      <AdminShell >
-        {children}
-      </AdminShell>
-    </>
-  );
+  return <AdminShell>{children}</AdminShell>;
 }
+
