1718edf7b63930c6c76ec20f73976a57e879996f
merajmehdizade <mdzmeraj8@gmail.com>
2026-02-06 01:41:57 +0330

    Fixed Bug & Add Loading For Btn and Pages &  Change NavDesign & Change AvatarDefualt

--- app/dashboard/student/_Components/AvatarNavItem.tsx
+++ app/dashboard/student/_Components/AvatarNavItem.tsx
@@ -3,14 +3,14 @@
 export default function AvatarNavItem({
     avatarSrc,
     avatarRef,
-    onClick,
+
 }: {
     avatarSrc: string;
     avatarRef: React.RefObject<HTMLDivElement | null>;
-    onClick: () => void;
+
 }) {
     return (
-        <div ref={avatarRef} onClick={onClick} className="w-8 h-8 rounded-full overflow-hidden cursor-pointer">
+        <div ref={avatarRef} className="w-8 h-8 rounded-full overflow-hidden cursor-pointer">
             <img src={avatarSrc} alt="avatar" className="w-full h-full object-cover" />
         </div>
     );
